250cf7352e8e9f5b03ad37adb9f52f3932824678
Damian Junko <damian.junko@gmail.com>
2016-10-06 20:56:18 +0200

    JavaScripts obsługa

---
+++ HomeApplication/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace HomeApplication.Controllers
+{
+    public class TestController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
