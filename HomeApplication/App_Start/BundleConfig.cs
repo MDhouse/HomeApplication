c8380650f36e911de1652ef180cca1da99527a98
Damian Junko <damian.junko@gmail.com>
2016-11-13 22:36:24 +0100

    Pobieranie danych z formularza

--- HomeApplication/App_Start/BundleConfig.cs
+++ HomeApplication/App_Start/BundleConfig.cs
@@ -2,9 +2,19 @@
 
 namespace HomeApplication
 {
+    /// <summary>
+    /// The bundle config.
+    /// </summary>
     public class BundleConfig
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+
+        /// <summary>
+        /// The register bundles.
+        /// </summary>
+        /// <param name="bundles">
+        /// The bundles.
+        /// </param>
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -13,7 +23,7 @@ namespace HomeApplication
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // Use the development version of Modernize to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
@@ -23,12 +33,12 @@ namespace HomeApplication
                       "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
-                "~/Scripts/script.js"   
-            ));
+                "~/Scripts/script.js",
+                "~/Scripts/input.mask.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/materialize/css/materialize.min.css",
-                      "~/Content/materialize/css/materialdesignicons.min.css",
+                      "~/Content/materialize/css/materialization.min.css",
                       "~/Content/site.css"));
         }
     }
--- HomeApplication/Scripts/script.js
+++ HomeApplication/Scripts/script.js
@@ -1,8 +1,59 @@
-﻿$(".button-collapse").sideNav();
+﻿
+function Form(formName) {
+    
+}
+
+
+// ReSharper disable once PossiblyUnassignedProperty
+$(".button-collapse").sideNav();
 $(document).ready(function () {
 
+
+    $("#data").mask("00/00/0000");
+    $("#telefon").mask("000 000 000");
+
+    $("#js-TestForm").on("click", "#js-FormTestSaveBtn", function() {
+        var dataForm = $("#js-formTestData").serializeArray();
+        var dataView = "";
+
+        for (var i = 0; i < dataForm.length; i++) {
+            if (i > 0 || i === dataForm.length) {
+                dataView += ", ";
+            }
+            dataView += dataForm[i].name + ": " + dataForm[i].value;
+        }
+        alert(dataView);
+    });
+
+
+
+    function clearInputs() {
+        var inputs = document.getElementsByTagName("input");
+            
+
+        for (var i = 0; i <= inputs.length; i++) {
+            console.log(inputs);
+            if (inputs[i].type === "text") {
+                console.log(inputs[i].name);
+                console.log(inputs[i].value);
+                inputs[i].val("");
+                inputs[i].onclick = function () { this.value = ""; }
+                console.log(inputs[i].value);
+            };
+        };
+    };
+
+
+
+    $("#js-TestForm").on("click", "#js-FormTestCancelBtn", function() {
+        clearInputs();
+            });
+
+
+
     ////Obsługa kalendarza
-        $('.datepicker').pickadate({
+// ReSharper disable once PossiblyUnassignedProperty
+        $(".datepicker").pickadate({
             onSet: function(arg) {
                 if ("select" in arg) {
                     this.close();
